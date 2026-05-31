039fe048983bb66acc79ebdd3440b13fef810324
nishchay <nishchay.connect.in@gmail.com>
2026-05-31 17:10:35 +0530

    models_update

--- Models.py
+++ Models.py
@@ -14,23 +14,33 @@ def modelInit(model,prompt):
 class LLM():
     
     def llama3(prompt):
-        return requests.post(url, json=modelInit("llama3.1:8b",prompt))
+        return requests.post(url, json=modelInit("llama3.1:8b",prompt)).json()["response"]
     
     def qwen3_5_4B(prompt):
-        return requests.post(url, json=modelInit("qwen3.5:4b",prompt))
+        return requests.post(url, json=modelInit("qwen3.5:4b",prompt)).json()["response"]
     
     def dolphin(prompt):
-        return requests.post(url, json=modelInit("dolphin3:8b",prompt))
+        return requests.post(url, json=modelInit("dolphin3:8b",prompt)).json()["response"]
     
     def qwen3_5_9B(prompt):
-        return requests.post(url, json=modelInit("qwen3.5:9b",prompt))
+        return requests.post(url, json=modelInit("qwen3.5:9b",prompt)).json()["response"]
     
     def medgemma(prompt):
-        return requests.post(url, json=modelInit("medgemma:4b",prompt))
+        return requests.post(url, json=modelInit("medgemma:4b",prompt)).json()["response"]
+    
+    def medgemma1_5(prompt):
+        return requests.post(url, json=modelInit("medgemma1.5:4b",prompt)).json()["response"]
     
     def gemma4(prompt):
-        return requests.post(url, json=modelInit("gemma4",prompt))
-    
-    
-    
+        return requests.post(url, json=modelInit("gemma4:e4B",prompt)).json()["response"]
 
+#### testing outputs
+   
+# response = LLM.gemma4("hi")
+# print(type(response))
+# print(response.status_code)
+# print(response.text)
+# print(response.json())
+
+# print(response.json()["response"])  
+    
